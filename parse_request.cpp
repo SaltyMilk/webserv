f2628991b0cab1c16df66a1455f7f6101866e245
Yocapell <yocapell@student.s19.be>
2020-09-21 22:16:11 +0200

    correction field_name

--- parse_request.cpp
+++ parse_request.cpp
@@ -70,31 +70,41 @@ int answer_request(int client_fd, t_req_line rl, t_net &snet)
 
 int	is_field_name(char *req_lines)
 {
-	if (!ft_strncmp("Host:", req_lines, 5))
-		return (1);
-	else if (!ft_strncmp("Connection:", req_lines, 11))
-		return (1);
-	else if (!ft_strncmp("Cache-Control:", req_lines, 14))
-		return (1);
-	else if (!ft_strncmp("Upgrade-Insecure-Requests:", req_lines, 26))
-		return (1);
-	else if (!ft_strncmp("User-Agent:", req_lines, 11))
-		return (1);
-	else if (!ft_strncmp("Accept:", req_lines, 7))
-		return (1);
-	else if (!ft_strncmp("Sec-Fetch-Site:", req_lines, 15))
-		return (1);
-	else if (!ft_strncmp("Sec-Fetch-Mode:", req_lines, 15))
-		return (1);
-	else if (!ft_strncmp("Sec-Fetch-User:", req_lines, 15))
-		return (1);
-	else if (!ft_strncmp("Sec-Fetch-Dest:", req_lines, 15))
-		return (1);
-	else if (!ft_strncmp("Accept-Encoding:", req_lines, 16))
+	if (!ft_strncmp("Accept-Charsets:", req_lines, 16))
 		return (1);
 	else if (!ft_strncmp("Accept-Language:", req_lines, 16))
 		return (1);
-	else if (!ft_strncmp("Cookie:", req_lines, 7))
+	else if (!ft_strncmp("Allow:", req_lines, 6))
+		return (1);
+	else if (!ft_strncmp("Authorization:", req_lines, 14))
+		return (1);
+	else if (!ft_strncmp("Content-Language:", req_lines, 17))
+		return (1);
+	else if (!ft_strncmp("Content-Length:", req_lines, 15))
+		return (1);
+	else if (!ft_strncmp("Content-Location:", req_lines, 17))
+		return (1);
+	else if (!ft_strncmp("Content-Type:", req_lines, 13))
+		return (1);
+	else if (!ft_strncmp("Date:", req_lines, 5))
+		return (1);
+	else if (!ft_strncmp("Host:", req_lines, 5))
+		return (1);
+	else if (!ft_strncmp("Last-Modified:", req_lines, 14))
+		return (1);
+	else if (!ft_strncmp("Location:", req_lines, 9))
+		return (1);
+	else if (!ft_strncmp("Referer:", req_lines, 8))
+		return (1);
+	else if (!ft_strncmp("Retry-After:", req_lines, 12))
+		return (1);
+	else if (!ft_strncmp("Server:", req_lines, 7))
+		return (1);
+	else if (!ft_strncmp("Transfer-Encoding:", req_lines, 18))
+		return (1);
+	else if (!ft_strncmp("User-Agent:", req_lines, 11))
+		return (1);
+	else if (!ft_strncmp("WWW-Authenticate:", req_lines, 17))
 		return (1);
 	return (0);
 }
@@ -123,8 +133,9 @@ std::string	field_name(char *req_line)
 
 int	get_in_map(t_req_line *rl, char **req_lines)
 { 
-	for(size_t i = 1; req_lines[i] && is_field_name(req_lines[i]);i++)
-		rl->field[field_name(req_lines[i])] = field_value(req_lines[i]);
+	for(size_t i = 1; req_lines[i];i++)
+		if (is_field_name(req_lines[i]))
+			rl->field[field_name(req_lines[i])] = field_value(req_lines[i]);
 	return (0);
 }
 

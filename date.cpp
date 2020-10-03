66e652024dca6b452cf889ac9b164a4e53d0e087
SaltyMilk <soufiane.elmelcaoui@gmail.com>
2020-10-03 06:38:18 +0200

    added RFC protection on last-modified header > date

--- date.cpp
+++ date.cpp
@@ -23,7 +23,13 @@ std::string get_last_modified(std::string filename)
 	struct stat st;
 	struct tm date;
 	stat(filename.c_str(), &st);
-	strptime(std::to_string(st.st_mtimespec.tv_sec).c_str(), "%s", &date);
+
+	time_t time = st.st_mtimespec.tv_sec;
+	struct timeval tp;
+	gettimeofday(&tp, nullptr);	
+	if (time > tp.tv_sec)//if last modified is in the future, we change it to the current time
+		time = tp.tv_sec;
+	strptime(std::to_string(time).c_str(), "%s", &date);
 	char buff[420];
 	strftime(buff, 420, "%a, %d %b %Y %H:%M:%S %Z", &date);
 	return (buff);
